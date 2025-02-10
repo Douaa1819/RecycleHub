879556469a1e4d65b398aedf397029ad5a601af3
Douaa <douaachemnane@gmail.com>
2025-02-10 13:26:23 +0100

    ajouter style pour home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,15 @@ import { EditRequestComponent } from './edit-request/edit-request.component';
 import { LogoutComponent } from './logout/logout.component';
 import { CollectorRequestsComponent } from './collector-requests/collector-requests.component';
 import { CollectorRequestDetailsComponent } from './collector-request-details/collector-request-details.component';
+import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'logout', component: LogoutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'collector-request', component: CollectorRequestsComponent },
   { path: 'collect-request', component: CollectRequestComponent },
   { path: 'collect-request-list',component: UserRequestsComponent},
